daaa36db163b2f39102ca5481345504a6d4d6286
Josh Lehr <joshua.valian17@gmail.com>
2026-02-03 16:12:55 -0700

    Add BasicML commands

---
+++ BasicML.py
@@ -0,0 +1,101 @@
+class BasicML:
+    def __init__(self):
+        pass
+
+    #TODO: Need to determine if we will "return accumulator" where the accumulator is required
+        # and assign that returned value to the int in CPU, or if we will use a list
+        # (which is auto-passed by reference).
+
+    
+    def read(memory, operand):
+        '''
+        Reads a word from the keyboard into a location in memory specified by the operand.
+        '''
+        read_input = input("Enter a 4-digit word: ") # Receives input from user.
+        memory[operand] = read_input # Stores input in memory.
+        
+
+    def write(memory, operand):
+        '''
+        Writes a word from a location in memory specified by the operand to screen.
+        '''
+        output = memory[operand] # Pulls the word from memory.
+        print(output) # Prints the word as the output.
+
+
+    def load(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Loads a word from a location in memory specified by the operand into the accumulator.
+        '''
+        loadValue = memory[operand] # Loads the value from memory.
+        accumulator = loadValue # Assigns the loaded value to the accumulator.
+
+
+    def store(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Store a word from the accumulator into a location in memory specified by the operand.
+        '''
+        memory[operand] = accumulator
+
+
+    def add(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Adds a word from a location in memory specified by the operand to the word in the accumulator
+        (leaves the result in the accumulator).
+        '''
+        accumulator += memory[operand]
+
+
+    def subtract(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Subtracts a word from a location in memory specified by the operand from the word in the accumulator
+        (leaves the result in the accumulator).
+        '''
+        accumulator -= memory[operand]
+
+    
+    def divide(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Divides the word in the accumulator by a word from a location specified by the operand in memory
+        (leaves the result in the accumulator).
+        '''
+        accumulator /= memory[operand]
+
+    def multiply(memory, operand, accumulator): #TODO: Accumulator fix
+        '''
+        Multiplies a word from a location in memory specified by the operand to the word in the accumulator
+        (leaves the result in the accumulator).
+        '''
+        accumulator *= memory[operand]
+
+    def branch(memory, operand): #TODO: Needs ability to set location/register.
+        '''
+        Branches to a location in memory specified by the operand.
+        '''
+        branchLocation = operand
+
+
+    def branchNeg(memory, operand, accumulator): #TODO: Accumulator fix
+                                                 #TODO: Needs ability to set location/register.
+        '''
+        Branches to a location in memory specified by the operand if the accumulator is negative.
+        '''
+        if accumulator < 0:
+            branchLocation = operand
+
+
+    def branchZero(memory, operand, accumulator): #TODO: Accumulator fix
+                                                  #TODO: Needs ability to set location/register.
+        '''
+        Branches to a location in memory specified by the operand if the accumulator is zero.
+        '''
+        if accumulator == 0:
+            branchLocation = operand
+
+
+    def halt(running): #TODO: Needs ability to set "running" to False
+        '''
+        Pauses the program.
+        '''
+        running = False
+    
