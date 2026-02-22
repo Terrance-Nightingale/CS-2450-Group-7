c820cd620bc2889c626da6e57c7b0a4f0566040b
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-02-21 21:10:22 -0700

    input info panel almost functional

--- GUI/InputInfoPanel.py
+++ GUI/InputInfoPanel.py
@@ -5,19 +5,27 @@ class InputInfoPanel:
     def __init__(self, container, interface_with_backend):
         self.input_to = interface_with_backend
         self.container = container
+        self.prev_inputs = ["These", "Are", "some", "inputs", "oh", "Three", "More"]
 
         tk.Label(
             self.container,
-            text="Previous inputs:"
-            ).grid(row=0, column=0)
+            text="Previous user inputs:"
+            ).pack(padx=5, pady=5)
 
         self.prev_inputs_label = tk.Label(
             self.container,
-            text="These\nAre\nsome\ninputs"
-            #textvariable=prev_inputs
+            text=""
         )
-        self.prev_inputs_label.grid(row=1, column=0, columnspan=2)
+        self.prev_inputs_label.pack(padx=5, pady=5)
+        self.update_prev_inputs_disp()
 
-    def update_prev_inputs(recent_input):
-        pass
+    def update_prev_inputs_disp(self):
+        output = ""
+        for line in self.prev_inputs:
+            output = output + line + "\n"
+        self.prev_inputs_label.config(text=output)
+
+    def add_prev_inputs(self, recent_input):
+        self.prev_inputs.insert(0, recent_input)
+        self.update_prev_inputs_disp()
 
