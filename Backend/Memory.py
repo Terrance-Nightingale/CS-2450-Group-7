425aa25386d42eb8ce3a51bb41009b2bde49db90
SamuelMB97 <43507135+SamuelMB97@users.noreply.github.com>
2026-03-21 23:41:53 -0600

    Fixed a bit of memory interaction with basicml

--- Backend/Memory.py
+++ Backend/Memory.py
@@ -2,11 +2,21 @@ class Memory:
 
     def __init__(self):
         self.memory_cap = 100
-        self._main_memory = []      
+        self._main_memory = []
+        self.just_loaded = False
 
-    #    for i in range(self.memory_cap): #initialize memory with all 0's
-    #        self._main_memory.append(0)
+        for i in range(self.memory_cap): #initialize memory with all 0's
+            self._main_memory.append(None)
             
     def main_memory(self):
         return self._main_memory
+    
+    def expand(self, operand):
+        while len(self._main_memory)-1 < operand:
+            self._main_memory.append(0)
+        print(self._main_memory)
+
+    def insert(self, operand, input):
+        self.expand(operand)
+        self._main_memory[operand] = input
     
