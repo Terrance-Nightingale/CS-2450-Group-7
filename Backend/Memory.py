9006ecaff5857dbd4a2156a1ab6fc1a8395761aa
EHUVU <11086684@uvu.edu>
2026-04-07 16:18:38 -0600

    Refactored memory

--- Backend/Memory.py
+++ Backend/Memory.py
@@ -2,11 +2,7 @@ class Memory:
 
     def __init__(self):
         self.memory_cap = 100
-        self._main_memory = []      
-
-    #    for i in range(self.memory_cap): #initialize memory with all 0's
-    #        self._main_memory.append(0)
+        self._main_memory = [0] * self.memory_cap
             
     def main_memory(self):
         return self._main_memory
-    
